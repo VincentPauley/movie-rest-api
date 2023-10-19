37b769c9fe0ec8096e266326fbce901ebb0487cf
Vinnie Pauley <vinniepauley@gmail.com>
2023-10-19 15:33:07 -0500

    get movie by id route

--- src/controllers/movies.ts
+++ src/controllers/movies.ts
@@ -4,6 +4,42 @@ import { Request, Response } from 'express'
 
 import MovieRatingOptions from '../constants/movie-ratings'
 
+// TODO: more performant route for movie fetching (does not require all fields.)
+
+// Find movie by ID.
+export const GetMovieById = (req: Request, res: Response) => {
+  let db = new sqlite3.Database('db.sqlite', (err) => {
+    if (err) {
+      return res.status(500).json({ message: 'Bad DB Connection'})
+    }
+
+    const { id } = req.params
+
+    db.all(`
+      SELECT
+        *
+      FROM
+        movies
+      WHERE
+        id = ?
+    `, [id], (err: any, rows: any) => {
+      if (err) {
+        return res.status(400).json({ message: 'Could not find record.'})
+      }
+
+      if (!Array.isArray(rows)) {
+        return res.status(400).json({ message: 'Could not find record.'})
+      }
+
+      if (rows.length !== 1) {
+        return res.status(400).json({ message: 'No record with that ID was found.'})
+      }
+
+      res.status(200).json({ movie: rows[0] })
+    })
+  })
+}
+
 export const AddMovie = (req: Request, res: Response) => {
   let db = new sqlite3.Database('db.sqlite', (err) => {
     if (err) {
--- src/routes/movies.ts
+++ src/routes/movies.ts
@@ -1,10 +1,11 @@
 import express from 'express'
-import { AddMovie, MovieRatings } from '../controllers/movies'
+import { AddMovie, GetMovieById, MovieRatings } from '../controllers/movies'
 
 const router = express.Router()
 
-router.post('/', AddMovie)
-
+router.get('/:id', GetMovieById)
 router.get('/ratings', MovieRatings)
+
+router.post('/', AddMovie)
 
 export default router
